8faa4a2b827549126d965a8ba76f5c2257b7f0ef
ix5231 <ix5231if@gmail.com>
2022-09-19 20:56:28 +0900

    Allow select picture

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,72 @@
 use std::path::PathBuf;
 
-use iced::{Alignment, Column, Element, Error, Image, Length, Sandbox, Settings};
+use iced::{
+    button, executor, Alignment, Application, Button, Column, Command, Element, Error, Image,
+    Length, Settings, Text,
+};
+use native_dialog::FileDialog;
 
 #[derive(Debug, Clone)]
 struct PicViewer {
-    pic_path: PathBuf,
+    pic_path: Option<PathBuf>,
+    explore_picture: button::State,
 }
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    OpenFile,
+}
 
-impl Sandbox for PicViewer {
+fn select_picture() -> Result<Option<PathBuf>, native_dialog::Error> {
+    FileDialog::new()
+        .add_filter("Pictures", &["jpg", "png", "bmp"])
+        .show_open_single_file()
+}
+
+impl Application for PicViewer {
     type Message = Message;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        PicViewer {
-            pic_path: PathBuf::from(r"./Lenna.bmp"),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            PicViewer {
+                pic_path: Option::None,
+                explore_picture: button::State::new(),
+            },
+            Command::none(),
+        )
     }
 
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
+    fn view(&mut self) -> Element<Self::Message> {
+        let mut controls = Column::new()
             .padding(10)
             .align_items(Alignment::Center)
-            .width(Length::Fill)
-            .push(Image::new(&self.pic_path))
+            .width(Length::Fill);
+
+        if let Some(pic_path) = &self.pic_path {
+            controls = controls.push(Image::new(pic_path));
+        }
+
+        controls
+            .push(
+                Button::new(&mut self.explore_picture, Text::new("test"))
+                    .on_press(Message::OpenFile),
+            )
             .into()
     }
 
-    fn update(&mut self, _message: Message) {}
+    fn update(&mut self, message: Message) -> Command<Self::Message> {
+        match message {
+            Message::OpenFile => {
+                let selected_pic = select_picture();
+                if let Ok(pic_path) = selected_pic {
+                    self.pic_path = pic_path;
+                }
+                Command::none()
+            }
+        }
+    }
 
     fn title(&self) -> String {
         "pic-viewer".to_string()
