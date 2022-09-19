21e2be731dd087bd344ad52cb6258f1d4f132fa2
ix5231 <ix5231if@gmail.com>
2022-09-19 10:14:59 +0900

    Show pic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 use std::path::PathBuf;
 
-use iced::Column;
+use iced::{Alignment, Column, Element, Error, Image, Length, Sandbox, Settings};
 
 #[derive(Debug, Clone)]
 struct PicViewer {
     pic_path: PathBuf,
 }
 
+#[derive(Debug)]
 enum Message {}
 
-impl PicViewer {
-    pub fn view(&self) -> Column<Message> {
-        column![Image::new(view.pic_path),]
+impl Sandbox for PicViewer {
+    type Message = Message;
+
+    fn new() -> Self {
+        PicViewer {
+            pic_path: PathBuf::from(r"./Lenna.bmp"),
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(10)
+            .align_items(Alignment::Center)
+            .width(Length::Fill)
+            .push(Image::new(&self.pic_path))
+            .into()
+    }
+
+    fn update(&mut self, _message: Message) {}
+
+    fn title(&self) -> String {
+        "pic-viewer".to_string()
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let mut settings = Settings::default();
+    settings.window.size = (640, 480);
+    PicViewer::run(settings)
 }
